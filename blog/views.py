f3e0e5ca046880b4f9b35b078b84aedb1b930b4e
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-12-01 18:33:30 -0600

    proyecto final - rama 2

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,3 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Post
-
-def post_detalle(request, anho, mes, dia, publicacion):
-    post = get_object_or_404(Post, slug=publicacion,
-                                   estado='publicado',
-                                   fechapub__year=anho,
-                                   fechapub__month=mes,
-                                   fechapub__day=dia)
-    return render(request, 'blog/post/detalle.html',
-                  {'post': post})
-
-class PostListView(ListView):
-    queryset= Post.publicaciones.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/lista.html'
