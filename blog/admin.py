3d239709aa5e75a744c753f1d064272c4139f131
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-12-01 18:30:25 -0600

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'slug', 'autor', 'fechapub', 'estado')
+    list_filter = ('estado', 'creacion', 'fechapub', 'autor')
+    search_fields = ('titulo', 'cuerpo')
+    prepopulated_fields = {'slug': ('titulo',)}
+    raw_id_fields = ('autor',)
+    date_hierarchy = 'fechapub'
+    ordering = ('estado', 'fechapub')
+        
+
+                    
